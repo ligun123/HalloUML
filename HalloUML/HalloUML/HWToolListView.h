d3315b114dd398e15b22c6dcf077db8c85159bc5
40k <li376438624@gmail.com>
2014-02-10 19:06:13 +0800

    add tool list view lkeft

---
+++ HalloUML/HalloUML/HWToolListView.h
@@ -0,0 +1,13 @@
+//
+//  HWToolListView.h
+//  HalloUML
+//
+//  Created by HalloWorld on 14-2-9.
+//  Copyright (c) 2014年 HalloWorld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HWToolListView : UIScrollView
+
+@end
---
+++ HalloUML/HalloUML/HWToolListView.m
@@ -0,0 +1,31 @@
+//
+//  HWToolListView.m
+//  HalloUML
+//
+//  Created by HalloWorld on 14-2-9.
+//  Copyright (c) 2014年 HalloWorld. All rights reserved.
+//
+
+#import "HWToolListView.h"
+
+@implementation HWToolListView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
